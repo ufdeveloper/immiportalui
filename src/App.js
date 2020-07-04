258378639e687ebff7b2aef60165c8d47ce48a7b
Shantanu Sardal <ufdeveloper@gmail.com>
2020-07-03 22:11:24 -0400

    added basic okta hosted login flow

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
 import Layout from "./components/Layout/Layout";
 import classes from './App.css';
+import { Security, LoginCallback, SecureRoute } from '@okta/okta-react';
 import {Switch, Route} from 'react-router-dom';
 import EmploymentHistory from "./components/EmploymentHistory/EmploymentHistory";
 import TravelHistory from "./components/TravelHistory/TravelHistory";
+import Homepage from "./containers/Homepage/Homepage";
+import Profile from "./components/Profile/Profile";
+
+const config = {
+    clientId: '0oajate0bv79bWV7P4x6',
+    issuer: 'https://dev-565937.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:3000/implicit/callback',
+    scopes: ['openid', 'profile', 'email'],
+    pkce: true
+};
 
 class App extends Component {
   render() {
@@ -11,11 +22,15 @@ class App extends Component {
       <div className={classes.App}>
         <Layout>
             <Switch>
-                <Route path="/employment" component={EmploymentHistory} />
-                <Route path="/travel" component={TravelHistory} />
-                <Route path="/education" component={EmploymentHistory} />
-                <Route path="/docs" component={EmploymentHistory} />
-                <Route path="/" component={EmploymentHistory} />
+                <Security {...config}>
+                    <SecureRoute path="/employment" component={EmploymentHistory} />
+                    <SecureRoute path="/travel" component={TravelHistory} />
+                    <SecureRoute path="/education" component={EmploymentHistory} />
+                    <SecureRoute path="/docs" component={EmploymentHistory} />
+                    <Route path="/implicit/callback" component={LoginCallback} />
+                    <SecureRoute path="/profile" component={Profile} />
+                    <Route path="/" exact component={Homepage} />
+                </Security>
             </Switch>
         </Layout>
       </div>
